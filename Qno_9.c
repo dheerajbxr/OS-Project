2fe04a3b068929ac694e92d6b2f9dee3418fcd6e
= <=>
2019-03-30 04:45:06 +0530

    Ques 9 completed

--- Qno_9.c
+++ Qno_9.c
@@ -20,4 +20,78 @@ temp[i] = b_time[i];
 }
 printf("\nEnter the Time Quantum:");
 scanf("%d", &time_quantum);
+printf("\nProcess ID\t\tBurst Time\t\t Turnaround Time\t\t Waiting Time\t Priority\n");
+for(total = 0, i = 0; x != 0;)
+{
+for(z=0;z<limit;z++)
+{
+int temp1;
+pos=z;
+for(j=z+1;j<limit;j++)
+{
+if(prio[j]<prio[pos])
+pos=j;
 }
+temp1=prio[z];
+prio[z]=prio[pos];
+prio[pos]=temp1;
+temp1=b_time[z];
+b_time[z]=b_time[pos];
+b_time[pos]=temp1;
+temp1=a_time[z];
+a_time[z]=a_time[pos];
+a_time[pos]=temp1;
+
+temp1=p[z];
+p[z]=p[pos];
+p[pos]=temp1;
+
+temp1=temp[z];
+temp[z]=temp[pos];
+temp[pos]=temp1;
+}
+{
+}
+if(temp[i] <= time_quantum && temp[i] > 0)
+{
+total = total + temp[i];
+temp[i] = 0;
+counter = 1;
+}
+else if(temp[i] > 0)
+{
+temp[i] = temp[i] - time_quantum;
+total = total + time_quantum;
+}
+for(b=0;b<limit;b++)
+{
+if(b==i)
+prio[b]+=1;
+else
+prio[b]+=2;
+}
+if(temp[i] == 0 && counter == 1)
+{
+x--;
+printf("\nProcess[%d]\t\t%d\t\t\t\t %d\t\t\t %d\t\t%d", p[i], b_time[i], total - a_time[i], total - a_time[i] - b_time[i],prio[i]);
+wait_time = wait_time + total - a_time[i] - b_time[i];
+turnaround_time = turnaround_time + total - a_time[i];
+counter = 0;
+}
+if(i == limit - 1)
+{
+i = 0;
+}
+else if(a_time[i + 1] <= total)
+{
+i++;
+}
+else
+{
+i = 0;
+
+}
+}
+return 0;
+}
+
