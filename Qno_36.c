ff73245824d78f71c37955f6753d0fe0dc9ee511
= <=>
2019-04-14 23:40:15 +0530

    Ques 36 Completed

--- Qno_36.c
+++ Qno_36.c
@@ -5,7 +5,19 @@ struct pro
 int id,brustTime;
 };
 struct pro Process[100];
-
+void sort(int n)
+{
+    for(int i=0;i<n-1;i++)
+	{
+        for(int j=0;j<n-i-1;j++){
+            if(Process[j].brustTime > Process[j+1].brustTime){
+                struct pro temp = Process[j];
+                Process[j] = Process[j+1];
+                Process[j+1]= temp;
+            }
+        }
+    }
+}
 void main() 
 {
     int n;
@@ -18,4 +30,18 @@ void main()
         Process[i].id = (i+1);
         totTime += Process[i].brustTime;
     }
+sort(n); 	
+    printf("\n");
+    printf("|");
+for(int i=0;i<n;i++){
+        for(int j=0;j<Process[i].brustTime-1;j++)
+            printf("-");
+        printf("|");
+    }  
+    
+    printf("\n");
+    for(int i=0;i<totTime+1;i++)
+        printf("-");
+	
 }
+
